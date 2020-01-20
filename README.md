e692079bd16473d6c1f40d15e19039fff50a1c90
linch90 <zlchu@outlook.com>
2020-01-20 11:31:46 +0800

    增加上传文件至HDFS示例

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 大数据学习示例
 ## HDFS
 1. [ReadFileFromHdfs](hdfs/src/main/java/com/linch/bigdata/hdfs/ReadFileFromHdfs.java)
+2. [WriteFileToHdfs](hdfs/src/main/java/com/linch/bigdata/hdfs/WriteFileToHdfs.java)
--- hdfs/src/main/java/com/linch/bigdata/hdfs/WriteFileToHdfs.java
+++ hdfs/src/main/java/com/linch/bigdata/hdfs/WriteFileToHdfs.java
@@ -1,32 +1,35 @@
 package com.linch.bigdata.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
 
 /**
  * @author linch90
  */
-public class ReadFileFromHdfs {
+public class WriteFileToHdfs {
     public static void main(String[] args) {
         if(args.length != 2) {
-            System.out.println("Usage: ReadFileFromHdfs <HdfsSrc> <LocalDest>");
+            System.out.println("Usage: WriteFileToHdfs <LocalSrc> <HdfsDest>");
             System.exit(1);
         }
+
         String src = args[0];
         String dest = args[1];
+
         Configuration conf = new Configuration();
-        try{
-            FileSystem fs = FileSystem.get(URI.create(src), conf);
-            FSDataInputStream in = fs.open(new Path(src));
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(dest));
+
+        try {
+            FileSystem fs = FileSystem.get(URI.create(dest), conf);
+            FSDataOutputStream out = fs.create(new Path(dest));
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(src));
             IOUtils.copyBytes(in, out, 4096, true);
         } catch (IOException e) {
             e.printStackTrace();
